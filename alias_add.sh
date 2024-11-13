362bc9bfd49766608c9816face1f61c88a7853c7
Kostiantyn Kuzin <audessie@gmail.com>
2024-11-13 07:39:44 +0200

    fixed: alias_add.sh ("source command issue")

--- alias_add.sh
+++ alias_add.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p -v "$HOME/bin"
+chmod u+x ./histrw.sh
 cp -v ./histrw.sh "$HOME/bin/"
 echo 'alias add="history' '|' "$HOME/bin/histrw.sh\"" >> "$HOME"/.bashrc
-# shellcheck source=/dev/null
-source "$HOME/.bashrc"
+echo -e "\nPlease run:\n  source ~/.bashrc\n\nor restart your terminal to activate the alias."
